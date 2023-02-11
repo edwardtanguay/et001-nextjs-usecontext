6d640395b0e8543804df2a341257697f8a523b7a
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-11 01:09:04 +0100

    base head

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,28 @@ export default function Head() {
       <title>Info Site</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta
+				property="og:title"
+				content="the site"
+			/>
+			<meta property="og:type" content="site" />
+			<meta
+				property="og:description"
+				content="site desc"
+			/>
+			<meta
+				property="og:image"
+				content="https://starters-backend.tanguay.eu/images/meta/site.png"
+			/>
+			<meta property="og:url" content="https://starters.tanguay.eu" />
     </>
   )
 }
