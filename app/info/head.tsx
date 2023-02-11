f6ec024d2dfc1622ad0f80f4f913a8764ac0d931
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-11 01:06:10 +0100

    meta info

--- app/info/head.tsx
+++ app/info/head.tsx
@@ -2,6 +2,28 @@ export default function Head({ params }: { params: { slug: string } }) {
 	return (
 		<>
 			<title>INFO PAGE</title>
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta
+				property="og:title"
+				content="THE INFO PAGE"
+			/>
+			<meta property="og:type" content="site" />
+			<meta
+				property="og:description"
+				content="This is a page about information, etc."
+			/>
+			<meta
+				property="og:image"
+				content="https://starters-backend.tanguay.eu/images/meta/site.png"
+			/>
+			<meta property="og:url" content="https://starters.tanguay.eu" />
 		</>
 	);
 }
