f681120584e0b37266a72151f44cea3c4bd47571
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-12 22:32:01 +0100

    info page in

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -3,9 +3,25 @@ import { useContext } from 'react';
 import { AppContext } from '../../AppContext';
 
 function Info() {
-	const { siteTitle } = useContext(AppContext);
+	const { siteTitle, jobs } = useContext(AppContext);
 
-	return <div>This is the info page for the site "{siteTitle}".</div>;
+	return (
+		<div className="page pageInfo">
+			<div>This is the info page for the site "{siteTitle}".</div>
+			<div className="content">
+				<div className="jobs">
+					<h2>{jobs.length} Jobs</h2>
+					{jobs.map((job) => {
+						return (
+							<div className="job" key={job.id}>
+								<div className="title">{job.title}</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Info;
